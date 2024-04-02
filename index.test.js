d9c0476c4c2ac70185fb9c2a6deaf8a033cf19c3
Simone Carriero <simonecarriero@gmail.com>
2024-04-02 16:42:05 +0200

    WIP

--- index.test.js
+++ index.test.js
@@ -1,6 +1,17 @@
-import { test, expect } from "vitest"
+import { test, expect } from "vitest";
 import { hello } from "./index";
 
 test("Hello, world!", () => {
-    expect(hello()).toBe("Hello, world!")
-})
+  expect(hello()).toBe("Hello, world!");
+});
+
+test("lift goes to the given floor when called", () => {
+  const lift = {
+    currentFloor: 5,
+    call: (floor) => {},
+  };
+
+  lift.call(5);
+
+  expect(lift.currentFloor).toBe(5);
+});
