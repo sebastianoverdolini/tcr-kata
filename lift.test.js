34d3359cbc253317db5d48dd50964db2c9a822ab
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2024-04-02 16:53:00 +0200

    WIP

--- lift.test.js
+++ lift.test.js
@@ -1,10 +1,5 @@
 import { test, expect } from "vitest";
-import { hello } from "./index";
 import { Lift } from "./lift";
-
-test("Hello, world!", () => {
-  expect(hello()).toBe("Hello, world!");
-});
 
 test("lift goes to the given floor when called", () => {
   const lift = new Lift();
